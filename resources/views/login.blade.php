b3932dd161cc5df7e30de09351145b53f417d2a4
hdinjos <hdinjos@gmail.com>
2022-09-10 21:30:18 +0700

    add view auth

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login Here</h2>
+    <form>
+        <label>username</label>
+        <input type="text"/>
+        <label>password</label>
+        <input type="text"/>
+        <button>Login</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login','App\Http\Controllers\AuthController@index')->name('login');
+
+Route::post('login_process','App\Http\Controllers\AuthController@login_process')->name('login_process');
+
+Route::get('logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::group(['middleware' => ['login_chechk:admin']], function(){
+        Route::resource('admin',AdminController::class);
+    });
+
+    Route::group(['middleware' =>['login_check:editor']], function(){
+        Route::resource('editor', AdminController::class);
+    });
 });
